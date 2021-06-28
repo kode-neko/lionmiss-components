fc52038712ae1b495b9cede0ef42ca981d90dc3c
Lady Sun <ladysun.freedom@gmail.com>
2021-06-28 16:07:37 +0200

    [develope] minimum base project working

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            Miau Miau!!!
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
+
+export default App
